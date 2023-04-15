73c4494ada72b6b4b76d17518a49630ed878e7d1
fraagu10 <aguilarfranco1092@protonmail.com>
2023-04-15 11:48:33 -0500

    added boostrap and basic layout for placement of modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RandomRecipeComponent } from './random-recipe/random-recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RandomRecipeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
