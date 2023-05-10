b52dd8d62064e116d97da163e5ffe3ed8f285def
Franco <aguilarfranco1092@protonmail.com>
2023-05-10 17:52:40 -0500

    seperated navigation bar into its own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RandomRecipeComponent } from './random-recipe/random-recipe.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RandomRecipeComponent
+    RandomRecipeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
